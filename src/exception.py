180e573f5d78405173bdc86c5d7adc7cb9fe5984
BhanuChandar2508 <polojubhanuchandar667@gmail.com>
2025-10-12 17:56:08 +0530

    Modified Logger and Exception code

--- src/exception.py
+++ src/exception.py
@@ -26,10 +26,10 @@ class CustomException(Exception):
     
 
 
-if __name__=='__main__':
-    try:
-        a=1/0
-
-    except Exception as e:
-        logging.info("Divided by Zero Error")
-        raise CustomException(e,sys)
+#if __name__=='__main__':
+#    try:
+#        a=1/0
+#
+#    except Exception as e:
+#        logging.info("Divided by Zero Error")
+#        raise CustomException(e,sys)
